b24464b753a68a23ef311ebb0ed0621ad180f4a3
Risa M <Risa@ayas-macbook-air.local>
2017-04-21 21:04:31 +0900

    Initial Commit

---
+++ MyCamera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyCamera
+//
+//  Created by Risa M on 2017/04/21.
+//  Copyright © 2017年 Risa M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
