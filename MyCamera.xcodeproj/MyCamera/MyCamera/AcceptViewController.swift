056ddefd7aa314b128f4a849bcf11c3bcbaf0f1d
RisaM <monkeylikey@gmail.com>
2017-04-22 15:19:34 +0900

    Add files via upload

---
+++ MyCamera.xcodeproj/MyCamera/MyCamera/AcceptViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AcceptViewController.swift
+//  MyCamera
+//
+//  Created by Risa M on 2017/04/22.
+//  Copyright © 2017年 Risa M. All rights reserved.
+//
+
+import UIKit
+
+class AcceptViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
